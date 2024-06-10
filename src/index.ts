90b42b2a9d05adc7241060a5d89b1606a11a59be
Wiktor Moskwa <wiktormosK@interia.pl>
2024-06-10 16:31:20 +0200

    Uploading Files

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import AWS from 'aws-sdk';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
+import * as fs from 'fs';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const envPath = path.resolve(__dirname, '../env/bucketPass.env');
@@ -12,7 +13,9 @@ const  S3Config = {
     accessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
 }
 
-//TODO na ENV !!!!!
+const Bucket = process.env.AWS_BUCKET_NAME
+const Prefix = process.env.AWS_PREFIX_NAME
+
 AWS.config.update({
     accessKeyId: S3Config.key,
     secretAccessKey: S3Config.accessKey
@@ -20,11 +23,11 @@ AWS.config.update({
 
 const s3 = new AWS.S3();
 
-const params = {
-    Bucket: 'developer-task',
-    Prefix: 'x-wing/'
-};
 async function listObjects() {
+    const params = {
+        Bucket: Bucket,
+        Prefix: Prefix
+    };
     try {
         const data = await s3.listObjectsV2(params).promise();
         console.log('Success', data);
@@ -33,5 +36,22 @@ async function listObjects() {
     }
 }
 
-// Call the function to list objects
+listObjects();
+
+const uploadFile = async (filePath: string, s3Key: string) => {
+    try {
+        const fileContent = fs.readFileSync( filePath, 'utf8');
+        const params = {
+            Bucket: Bucket,
+            Key: Prefix,
+            Body: fileContent
+        };
+        await s3.upload(params).promise();
+        console.log(`File uploaded successfully: ${s3Key}`);
+    } catch (err) {
+        console.error('Error uploading file:', err);
+    }
+};
+
+uploadFile('./TestUpload.txt',"LoremIpsum")
 listObjects();
